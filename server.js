0a5be2602b32f331c1074a14da47c9d45a4286f9
Deepak <deepakd@acevin.com>
2025-08-25 13:01:53 +0000

    Adding the new route in the code

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) =>{
   res.send('Hello from CI CD dummy project')
 })
 
+app.get('/api/users', (req, res) =>{
+  res.send('Hello from CI CD dummy users and new route')
+})
+
+
 
 app.listen(PORT, ()=>{
   console.log(`Server is running in the prot ${PORT}`)
