125b3f536fda18e3bff01c0dccc02d9446170b27
Deepak <deepakd@acevin.com>
2025-08-25 11:48:41 +0000

    Adding the CI CD code in the server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) =>{
+  res.send('Hello from CI CD dummy project')
+})
+
+
+app.listen(PORT, ()=>{
+  console.log(`Server is running in the prot ${PORT}`)
+})
